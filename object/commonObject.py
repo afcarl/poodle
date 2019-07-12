e3fc04079c4ee523635a887e2a356ce3ddb351d3
Artem <amaximovich@gmail.com>
2019-07-12 20:27:16 +0000

    bug

--- object/commonObject.py
+++ object/commonObject.py
@@ -6,3 +6,16 @@ Number.higher_than = Relation(Number)
 
 class Number4Bit(Imaginary):
     pass
+
+         
+class AddedNumber(Object):
+    cost = 1
+    operator1 = Property(Number)
+    operator2 = Property(Number)
+    result = Property(Number)
+
+class GreaterThan(Object):
+
+    lower = Property(Number)
+    higher = Property(Number)
+
