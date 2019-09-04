097989f234a82d1628f8a20c02653f52773381fb
Andrew Gree <andrew@xhop.ai>
2019-09-04 02:51:16 +0000

    experimental full string equivalence support

--- poodle_lib/string.py
+++ poodle_lib/string.py
@@ -1,7 +1,36 @@
 import poodle
 from .poodle_main import resolve_poodle_special_object, Property, new_internal
 
+
+class _StringFactory:
+    def __init__(self):
+        self.reset()
+    def get(self, value):
+        if not value in self.values:
+            self.values[value] = new_internal(String, value) # needed to indicate internal call
+        return self.values[value]
+    def get_objects(self):
+        return list(self.values.values())
+    def reset(self):
+        self.values = {}
+
+
+stringFactory = _StringFactory()
+
+
 class String(poodle.Object):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.poodle_internal__value in stringFactory.values:
+            self.poodle_internal__sym_name = stringFactory.values[self.poodle_internal__value].poodle_internal__sym_name
+    def __setattr__(self, name, value):
+        if not name.startswith("_") and not name.startswith("poodle_internal"): 
+            raise TypeError("Strings are immutable")
+        return super().__setattr__(name, value)
+    def __getattr__(self, name):
+        if not name.startswith("_") and not name.startswith("poodle_internal"): 
+            raise TypeError("Strings are immutable")
+        return super().__getattr__(name, value)
     def __str__(self):
         return str(self._get_value())
     def __repr__(self):
@@ -20,16 +49,3 @@ class String(poodle.Object):
             return self.poodle_internal__value == other.poodle_internal__value
         return super().__eq__(other)
 
-class _StringFactory:
-    def __init__(self):
-        self.reset()
-    def get(self, value):
-        if not value in self.values:
-            self.values[value] = new_internal(String, value) # needed to indicate internal call
-        return self.values[value]
-    def get_objects(self):
-        return list(self.values.values())
-    def reset(self):
-        self.values = {}
-
-stringFactory = _StringFactory()
